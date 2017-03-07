e5012a125452336d7a7882b3a2be98e0b1d0af8d
Harshit Maheshwari <hmharshit@gmail.com>
2017-03-07 22:31:32 +0530

    FEAT: Dialpad in Contact Activity

--- app/src/main/java/com/party/parthverma/collegeapp/Contact.java
+++ app/src/main/java/com/party/parthverma/collegeapp/Contact.java
@@ -1,7 +1,9 @@
 package com.party.parthverma.collegeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Contact extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class Contact extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
     }
+
 }
