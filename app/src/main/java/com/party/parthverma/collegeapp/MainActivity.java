05ef5ff84f7085f4085164a78399270fd365a970
Harshit Maheshwari <hmharshit@gmail.com>
2017-03-11 14:12:06 +0530

    Basic Design of About Activity

--- app/src/main/java/com/party/parthverma/collegeapp/MainActivity.java
+++ app/src/main/java/com/party/parthverma/collegeapp/MainActivity.java
@@ -4,16 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        about = (Button) findViewById(R.id.collapsingToolbar);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, About.class));
+            }
+        });
     }
     public void goToActivityContact (View view) {
         Intent intent = new Intent(this, Contact.class);
         startActivity(intent);
     }
+
+
 }
