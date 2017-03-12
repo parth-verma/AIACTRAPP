d7bff54c18ec356e46b05fc237ac918de5e395a9
Parth Verma <v.parth98@gmail.com>
2017-03-12 18:01:11 +0530

    Added action bar to Clubs page

--- app/src/main/java/com/party/parthverma/collegeapp/Clubs.java
+++ app/src/main/java/com/party/parthverma/collegeapp/Clubs.java
@@ -1,7 +1,9 @@
 package com.party.parthverma.collegeapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +16,15 @@ public class Clubs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clubs);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Clubs");
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
         listView = (ListView)findViewById(R.id.list_of_clubs);
         final ArrayList<Club> clubList = Club.getClubs(this);
         ClubsCardAdapter adapter = new ClubsCardAdapter(this,clubList);
