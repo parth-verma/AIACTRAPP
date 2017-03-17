2cdd1820d674ac4d02b2e96af9b868ffebb0a8de
Harshit Maheshwari <hmharshit@gmail.com>
2017-03-17 21:01:29 +0530

    Removed Card View and created JSON file in Assets Folder

--- app/src/main/java/com/party/parthverma/collegeapp/About.java
+++ app/src/main/java/com/party/parthverma/collegeapp/About.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,6 +32,7 @@ public class About extends AppCompatActivity  {
         collapsingToolbarLayout.setContentScrimColor(ContextCompat.getColor(context,R.color.colorPrimary));
         ImageView imageView = (ImageView) findViewById(R.id.bgheader);
         Picasso.with(this).load("http://www.planwallpaper.com/static/images/HD-Wallpapers1_FOSmVKg.jpeg").into(imageView);
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -41,6 +44,7 @@ public class About extends AppCompatActivity  {
         Intent intent = new Intent(this, Contact.class);
         startActivity(intent);
     }
+
 
 }
 
