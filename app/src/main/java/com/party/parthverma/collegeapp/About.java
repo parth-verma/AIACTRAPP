6103d3f36953eadd2ce147c97056dda1bd5fac86
Harshit Maheshwari <hmharshit@gmail.com>
2017-03-17 13:15:19 +0530

    About Activity Completed except the pinning of Fab Button

--- app/src/main/java/com/party/parthverma/collegeapp/About.java
+++ app/src/main/java/com/party/parthverma/collegeapp/About.java
@@ -1,17 +1,20 @@
 package com.party.parthverma.collegeapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,10 @@ public class About extends AppCompatActivity {
         if(item.getItemId()==android.R.id.home)
             finish();
         return super.onOptionsItemSelected(item);
+    }
+    public void goToActivityContact1(View view) {
+        Intent intent = new Intent(this, Contact.class);
+        startActivity(intent);
     }
 
 }
