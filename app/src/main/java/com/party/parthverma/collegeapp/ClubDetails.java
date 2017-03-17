4bd060cee321c4ad97c132ccab712e0dd0b69465
Parth Verma <v.parth98@gmail.com>
2017-03-17 21:28:42 +0530

    FEAT: Clubs listing completed

--- app/src/main/java/com/party/parthverma/collegeapp/ClubDetails.java
+++ app/src/main/java/com/party/parthverma/collegeapp/ClubDetails.java
@@ -1,36 +1,43 @@
 package com.party.parthverma.collegeapp;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ClubDetails extends AppCompatActivity {
 
-    TextView title, type, desc;
+    TextView desc;
+    Toolbar toolbar;
+    ImageView clubImage;
+    CollapsingToolbarLayout collapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_details);
-        title = (TextView) findViewById(R.id.club_name);
-        type = (TextView) findViewById(R.id.club_type);
-        desc = (TextView) findViewById(R.id.club_desc);
-
         int position = this.getIntent().getExtras().getInt("position");
-
         Club club = Club.getClubs(this).get(position);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.club_detail_toolbar);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setTitle(club.name);
+        desc = (TextView) findViewById(R.id.club_desc);
+        clubImage = (ImageView) findViewById(R.id.club_image);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.CollapsingToolbarLayout1);
+
+        setSupportActionBar(toolbar);
+
+        collapsingToolbarLayout.setTitle(club.name);
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
-        title.setText(club.name);
-        type.setText(club.type);
         desc.setText(club.desc);
+        Picasso.with(this).load(club.image_loc).placeholder(R.drawable.output).into(clubImage);
 
 
 
