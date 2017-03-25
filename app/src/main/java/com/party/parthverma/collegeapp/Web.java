cefafc644494af1fa4613878d8d9cf3a554c0817
Harshit Maheshwari <hmharshit@gmail.com>
2017-03-25 16:27:58 +0530

    Working URL link

--- app/src/main/java/com/party/parthverma/collegeapp/Web.java
+++ app/src/main/java/com/party/parthverma/collegeapp/Web.java
@@ -20,7 +20,7 @@ public class Web extends AppCompatActivity {
 
         WebView webview = (WebView) findViewById(R.id.webView1);
         webview.getSettings().setJavaScriptEnabled(true);
-        String ece1 = "https://firebasestorage.googleapis.com/v0/b/aiactrapp.appspot.com/o/Syllabus%20CSE%2Fcse%201.pdf?alt=media&token=d1e36cb7-e993-4656-8d40-75c0eab26ab7";
-        webview.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" + ece1);
+        String pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+        webview.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
     }
 }
