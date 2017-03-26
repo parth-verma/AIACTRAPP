98628179f48e445cde6862e0986126bac3e8195f
Harshit Maheshwari <hmharshit@gmail.com>
2017-03-26 22:53:52 +0530

    Splash Screen is moved to Full Screen

--- app/src/main/java/com/party/parthverma/collegeapp/SplashScreen.java
+++ app/src/main/java/com/party/parthverma/collegeapp/SplashScreen.java
@@ -3,6 +3,8 @@ package com.party.parthverma.collegeapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,6 +14,8 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         Animation animation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim);
