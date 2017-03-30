54d257f0b3a8bd5180cdfbac83ddac0473257360
Parth Verma <v.parth98@gmail.com>
2017-03-30 12:04:51 +0530

    WIP: Downloader for syllabus

--- app/src/main/java/com/party/parthverma/collegeapp/CseFragment.java
+++ app/src/main/java/com/party/parthverma/collegeapp/CseFragment.java
@@ -1,13 +1,25 @@
 package com.party.parthverma.collegeapp;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.thin.downloadmanager.DefaultRetryPolicy;
+import com.thin.downloadmanager.DownloadRequest;
+import com.thin.downloadmanager.DownloadStatusListenerV1;
+import com.thin.downloadmanager.ThinDownloadManager;
 
 /**
  * Created by harshit on 27/3/17.
@@ -17,6 +29,7 @@ public class CseFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
     ListView listView;
+    private ThinDownloadManager downloadManager;
 
 
     public CseFragment() {
@@ -35,7 +48,7 @@ public class CseFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_cse, container, false);
         listView = (ListView) rootView.findViewById(R.id.cse_syllabus_list);
-
+        downloadManager = new ThinDownloadManager();
         String[] values = new String[]{"Semester 1", "Semester 2", "Semester 3", "Semester 4", "Semester 5", "Semester 6", "Semester 7", "Semester 8"};
 
         ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, values);
@@ -43,8 +56,42 @@ public class CseFragment extends Fragment {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //start download here
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReference();
+                Log.d("DOWNLOAD", Integer.toString(position));
+                storageRef.child("/Syllabus CSE/cse " + Integer.toString(position + 1) + ".pdf").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d("DOWNLOAD", uri.toString());
+                        Uri destinationUri = Uri.parse(getContext().getExternalCacheDir().toString() + "/cse" + Integer.toString(position + 1) + ".pdf");
+                        DownloadRequest downloadRequest = new DownloadRequest(uri)
+                                .setRetryPolicy(new DefaultRetryPolicy())
+                                .setDestinationURI(destinationUri)
+                                .setStatusListener(new DownloadStatusListenerV1() {
+                                    @Override
+                                    public void onDownloadComplete(DownloadRequest downloadRequest) {
+                                        Log.d("DOWNLOAD", "complete");
+                                        Snackbar.make(getView().findViewById(android.R.id.content), "Downloaded CSE " + Integer.toString(position + 1) + " Syllabus", Snackbar.LENGTH_LONG)
+                                                .setAction("Open", mOnClickListener)
+                                                .setActionTextColor(Color.RED)
+                                                .show();
+                                    }
+
+                                    @Override
+                                    public void onDownloadFailed(DownloadRequest downloadRequest, int errorCode, String errorMessage) {
+                                        Log.d("DOWNLOADError", errorMessage);
+                                    }
+
+                                    @Override
+                                    public void onProgress(DownloadRequest downloadRequest, long totalBytes, long downloadedBytes, int progress) {
+                                        Log.d("Progress: ", Integer.toString(progress));
+                                    }
+                                });
+                        downloadManager.add(downloadRequest);
+                    }
+                });
+
             }
         });
 
